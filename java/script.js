8616cbdaa0593ee70567a1fb8de13d14de2df144
keerthna-unnikrishnan <144515493+keerthna-unnikrishnan@users.noreply.github.com>
2023-10-07 15:23:46 +0530

    Add files via upload

---
+++ java/script.js
@@ -0,0 +1,178 @@
+// console.log('hello')
+// // alert('warning')
+// // variables
+// var a=8;
+// var a=10;
+// console.log(a);//block 1
+
+// {
+//      var a=20;
+//      console.log(a);         //block 2
+// }
+// let b=23;
+// console.log(b);
+// {
+//     let b=34;
+//     console.log(b);
+// }
+// b=44;
+// console.log(b);
+// // const
+// const t=30;
+// console.log(t);
+// {
+//     const t=90;
+//     console.log(t);
+// }
+//     console.log(t);
+//     //data types
+//     var nam='today was a good day and also a beautiful day huhuhuhuhuhu';
+//     console.log(typeof(nam));
+//     //Numbers
+//     var count=30;
+//     console.log(typeof(count));
+//     //boolean
+//     var bb=true;
+//     console.log(typeof(bb));
+//     var gtype;
+//     console.log(typeof(gtype));
+//     //Arrays
+//     let arr=['blue','red','white',10]
+//     console.log(arr[1]);
+//     console.log(arr.length);
+//     arr.push('violet');
+//     console.log(arr);
+//     arr.pop()
+//     console.log(arr);
+//     //javascripts objects
+//     let car={
+//         mileage:100,
+//         model:'i10',
+//         color:'red'
+//     }
+//     console.log(car)
+//     console.log(car.model);
+//     console.log(car.mileage);
+//     let person=new Object();
+//     person.gender='male';
+//     person.height=180;
+//     console.log(person);
+//     //variables
+//     var c=5, d=10;
+//     console.log(c+d);
+    
+// var a=10;
+// var b=++a;
+// console.log(b);
+// console.log(a);
+
+
+//var a=10;
+//var b=a++;
+//console.log(b);
+//console.log(a);
+// if (a==b) {
+//     console.log('a is equal to be')
+    
+// } else {
+//     console.log('a is not equal to b')
+// }
+// if (a==b) {
+//     console.log('a is equal to b')
+    
+// } else {
+//     console.log('a is not equal to b')
+    
+// }
+//if (a==b && a==10) {
+ //   console.log('a is equal to b')
+    
+// else {
+ //   console.log('a is not equal to b and a is not 10')
+    
+//}
+// var c=10;
+// var d=12;
+// if (c===d) {
+//     console.log('c equal to d')
+    
+// } else {
+//     console.log('c not equal to d')
+    
+// }
+// var c=('10');
+// var d=10;
+// if (c==d) {
+//     console.log('c equal to d')
+    
+// } else {
+//     console.log('c not equal to d')
+    
+// }
+
+//  var e=10;
+//  var f=15;
+// if (e==f && e==20) {
+//     console.log('e is greater than f')  
+// } else {
+//     console.log('e is smaller than f')
+    
+// }
+
+//  let add=6;
+// add += 3;
+//  console.log(add);
+
+// let s=6;
+// s -= 3;
+// console.log(s);
+
+let A=10;
+let B=34;
+let C=70;
+let D=12;
+var exp=A+B*(C-D)-7;
+console.log(exp);
+
+ //functions
+ function display(name){
+    console.log('hello  '+name+'  welcome to js');
+ }
+ display('keer');//function call
+
+ function addition(t,r){
+    var result=t+r;
+  return result;
+ }
+ var result=addition(10,30);
+ console.log('the sum is'+result);
+
+ function sub(j,k){
+    var result=j-k;
+  return result;
+ }
+ var result=sub(10,30);
+ console.log('the sub is'+result);
+
+ function mul(f,z){
+    var result=f*z;
+  return result;
+ }
+ var result=mul(10,30);
+ console.log('the mul is'+result);
+ 
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+    
+
+
